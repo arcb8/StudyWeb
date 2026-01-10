4444a3f810fb2a54ce694b2a56257d7615201a2d
arcb8 <arcb8@gmail.com>
2026-01-10 12:40:05 +0200

    add db connection

---
+++ StudyWeb/Repositories/Books/BookDbRepository.cs
@@ -0,0 +1,55 @@
+﻿using StudyWeb.Data;
+using StudyWeb.Entities;
+
+namespace StudyWeb.Repositories;
+
+public class BookDbRepository : IBookRepository
+{
+    private AppDbContext _dbContext;
+
+    public BookDbRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public List<Book> GetAll()
+    {
+        var books = _dbContext.Books.ToList();
+        return books;
+    }
+
+    public Book Get(int id)
+    {
+        var books = GetAll();
+        foreach (var book in books)
+        {
+            if (book.Id == id)
+            {
+                return book;
+            }
+        }
+
+        return null;
+    }
+
+    public Book Create(Book book)
+    {
+        _dbContext.Books.Add(book);
+        _dbContext.SaveChanges();
+        return book;
+    }
+
+    public Book Update(int id, Book book)
+    {
+        _dbContext.Books.Update(book);
+        _dbContext.SaveChanges();
+        return book;
+    }
+
+    public int Delete(int id)
+    {
+        var book = Get(id);
+        _dbContext.Books.Remove(book);
+        _dbContext.SaveChanges();
+        return id;
+    }
+}
